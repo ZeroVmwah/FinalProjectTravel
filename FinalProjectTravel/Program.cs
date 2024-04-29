2da6419be1132fc270d1057e00b137613bd12684
ZeroVmwah <124622710+ZeroVmwah@users.noreply.github.com>
2024-04-29 12:18:57 +0300

    Update Program.cs

--- FinalProjectTravel/Program.cs
+++ FinalProjectTravel/Program.cs
@@ -11,7 +11,7 @@ namespace FinalProjectTravel
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Yourtrips());
+            Application.Run(new SignUp());
         }
     }
 }
